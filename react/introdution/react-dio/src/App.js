3b3f02c1636767fb882cf1c0579d72117c505cbc
AltherLago <ajsl@ic.ufal.br>
2022-08-19 13:01:48 -0300

    first react

---
+++ react/introdution/react-dio/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+  return(
+    <>
+      <h1>My first App with React</h1>
+      <ul>
+        <a href="/" className="list-group-item list-group-item-action list-group-item-dark"> Item 2 </a>
+        <a href="/" className="list-group-item list-group-item-action list-group-item-dark"> Item 3 </a>
+      </ul>
+    </>
+  )
+}
+
+export default App;
+
+
