d05a9431da03e9daff27755c7738e2ea5faed300
AltherLago <ajsl@ic.ufal.br>
2022-08-26 00:17:14 -0300

    UncheckedException

--- java/introduction/exceptions/src/UncheckedException.java
+++ java/introduction/exceptions/src/UncheckedException.java
@@ -1,5 +1,3 @@
-package src;
-
 import javax.swing.*;
 
 public class UncheckedException {
@@ -13,11 +11,25 @@ public class UncheckedException {
             String a = JOptionPane.showInputDialog("Numerator: ");
             String b = JOptionPane.showInputDialog("Denominator: ");
             try{
-                int resul
+                int result = division(Integer.parseInt(a), Integer.parseInt(b));
+                System.out.println("Resultado: " + result);
+                continueLooping = false;
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, " Invalid input, enter a integer! " + e.getMessage());
+            } catch (ArithmeticException e){
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Impossible to divide by 0.");
             }
-        }
+            finally {
+                System.out.println("Finish");
+            }
+        } while(continueLooping);
     }
 
+    public static int division(int a, int b){
+        return a / b;
+    }
     public static void cleanConsoleScreen(){
 
         System.out.print("[H[2J");
