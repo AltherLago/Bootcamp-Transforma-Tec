c35a47ddadb19908d30d94fa8d4513d6d193930c
AltherLago <ajsl@ic.ufal.br>
2022-08-25 23:56:19 -0300

    start exceptions

---
+++ java/introduction/exceptions/src/UncheckedException.java
@@ -0,0 +1,28 @@
+package src;
+
+import javax.swing.*;
+
+public class UncheckedException {
+
+    public static void main(String[] args){
+
+        cleanConsoleScreen();
+
+        boolean continueLooping = true;
+        do {
+            String a = JOptionPane.showInputDialog("Numerator: ");
+            String b = JOptionPane.showInputDialog("Denominator: ");
+            try{
+                int resul
+            }
+        }
+    }
+
+    public static void cleanConsoleScreen(){
+
+        System.out.print("[H[2J");
+        System.out.flush();
+
+    }
+
+}
