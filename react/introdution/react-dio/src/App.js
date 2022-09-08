57bd82608f573504a3701de2b0398ad98bdb6028
AltherLago <ajsl@ic.ufal.br>
2022-09-07 23:32:27 -0300

    react intro.2

--- react/introdution/react-dio/src/App.js
+++ react/introdution/react-dio/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import Item from './components/Item';
 
 const App = () => {
   return(
     <>
       <h1>My first App with React</h1>
       <ul>
-        <a href="/" className="list-group-item list-group-item-action list-group-item-dark"> Item 2 </a>
-        <a href="/" className="list-group-item list-group-item-action list-group-item-dark"> Item 3 </a>
+        <Item>
+          Item 1
+        </Item>
+        <Item>
+          Item 2
+        </Item>
+         <Item>
+          Item 3
+        </Item>
+  
+
       </ul>
     </>
   )
