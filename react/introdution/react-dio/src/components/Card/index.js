572448374e40cebe2ff158090e950d37012b383a
AltherLago <ajsl@ic.ufal.br>
2022-09-09 00:25:54 -0300

    react.4

--- react/introdution/react-dio/src/components/Card/index.js
+++ react/introdution/react-dio/src/components/Card/index.js
@@ -1,13 +1,35 @@
+import { useState } from "react";
+import Button from "../Button";
+
 const Card = () =>{
+
+    const [value, setValue] = useState(0)
+
+    function Adction(){
+        setValue(value + 1)
+    }
+    
+    function Remove(){
+        setValue(value - 1)
+    }
+
     return(
         <div className="card">
             <div className="card-header">
                 My first Card
             </div>
             <div className="card-body">
-                <button type="button" className="btn btn-success">Adiction</button>
-                <button type="button" class="btn btn-danger">Remove</button>
-                <p>0</p>
+                <Button 
+                    className="btn btn-success"
+                    onClick={Adction}>
+                        Adiction
+                </Button>
+                <Button 
+                    className="btn btn-danger"
+                    onClick={Remove}>
+                        Remove
+                </Button>
+                <p>{value}</p>
 
             </div>
         </div>
